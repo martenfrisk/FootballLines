bd281126b214f3bfba197d05a33dcb8d9835e42e
Marten <marten.frisk@gmail.com>
2020-06-13 23:43:05 +0200

    Updates

---
+++ precache-manifest.f072d2f1cf2144c67352623a790571cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d544fe2591ce3db0d43b488bb495b0",
+    "url": "/FootballLines/index.html"
+  },
+  {
+    "revision": "e63cf39352cf47f25dc2",
+    "url": "/FootballLines/static/css/main.dbd54d61.chunk.css"
+  },
+  {
+    "revision": "de9ded5030fbe12fb069",
+    "url": "/FootballLines/static/js/2.fc189d03.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/FootballLines/static/js/2.fc189d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e63cf39352cf47f25dc2",
+    "url": "/FootballLines/static/js/main.7d64c074.chunk.js"
+  },
+  {
+    "revision": "a3e1b640f54200fce984",
+    "url": "/FootballLines/static/js/runtime-main.01f81803.js"
+  }
+]);
